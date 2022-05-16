4c8e65c03ffe7090091ee2977146b061ee9e9e93
Andrew Goetz <agoetz4407@gmail.com>
2022-05-16 17:38:55 -0500

    starter database setup

---
+++ db/db.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS get_team_db;
+CREATE DATABASE get_team_db;
+USE get_team_db;
---
+++ index.js
@@ -0,0 +1,8 @@
+const inquirer = require('inquirer');
+const db = require('./db/connection');
+
+db.connect(err => {
+    if(err) throw err;
+    console.log('Database connected');
+})
+
