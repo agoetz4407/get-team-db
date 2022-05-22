704456e174028a8c499aaefc11f2053fe2814ceb
Andrew Goetz <agoetz4407@gmail.com>
2022-05-22 18:44:25 -0500

    all 3 view functions working correctly

--- index.js
+++ index.js
@@ -53,6 +53,7 @@ const viewDepartments = () => {
     const sql = 'SELECT * FROM departments'
     db.query(sql, (err, rows) => {
         if (err) throw err;
+        console.log('\n');
         console.table(rows);
         actionPrompt();
     })
@@ -68,14 +69,46 @@ const viewRoles = () => {
     LEFT JOIN departments ON roles.department_id = departments.id;`
     db.query(sql, (err, rows) => {
         if (err) throw err;
+        console.log('\n');
         console.table(rows);
         actionPrompt();
     })
 };
 
-const viewEmployees = () => {};
+const viewEmployees = () => {
+    const sql = `SELECT 
+    employees.id,
+    CONCAT(employees.first_name, ' ', employees.last_name) AS 'name',
+    departments.name AS 'department',
+    roles.title,
+    roles.salary,
+    CONCAT(manager.first_name, ' ', manager.last_name) AS 'manager'
+    FROM employees
+    INNER JOIN roles ON employees.role_id = roles.id
+    LEFT JOIN departments ON roles.department_id = departments.id
+    LEFT JOIN employees manager ON employees.manager_id = manager.id
+    ORDER BY employees.id;`
+    db.query(sql, (err, rows) => {
+        if (err) throw err;
+        console.log('\n');
+        console.table(rows);
+        actionPrompt();
+    })
+};
 
-const addDepartment = () => {};
+const addDepartment = () => {
+    inquirer.prompt(addDepartmentQuestion)
+    .then(answer => {
+        sql = 'INSERT INTO departments (name) VALUES (?)';
+        params = answer.departmentName;
+        db.query(sql, params, (err, rows) => {
+            if (err) throw err;
+            console.log('\n');
+            console.table(rows);
+            actionPrompt();
+        })
+    })
+};
 
 const addRole = () => {};
 
